4ad80c3697168300a1d632d6db17ff840cc375cc
camilagarcia113 <camilagarcia.113@gmail.com>
2016-03-24 20:16:37 -0300

    Formatted files

--- desapp-groupb-backend/src/test/model/VehicleTest.java
+++ desapp-groupb-backend/src/test/model/VehicleTest.java
@@ -7,28 +7,28 @@ import org.junit.Test;
 
 public class VehicleTest {
 
-	public Vehicle vehicle;
-	
-	@Before
-	public void setUp() {
-		vehicle = new Vehicle(4);
-	}
-	
-	@Test
-	public void itShouldKnowItsCapacity() {
-		assertEquals(vehicle.getCapacity(), 4);
-	}
-	
-	@Test
-	public void itShouldBeAbleToReceiveGoodRates() {
-		vehicle.receiveGoodRate();
-		vehicle.receiveGoodRate();
-		assertEquals(vehicle.getRate(), 2);
-	}
-	
-	@Test
-	public void itShouldBeAbleToReceiveBadRates() {
-		vehicle.receiveBadRate();
-		assertEquals(vehicle.getRate(), -1);
-	}
+    public Vehicle vehicle;
+
+    @Before
+    public void setUp() {
+        vehicle = new Vehicle(4);
+    }
+
+    @Test
+    public void itShouldKnowItsCapacity() {
+        assertEquals(vehicle.getCapacity(), 4);
+    }
+
+    @Test
+    public void itShouldBeAbleToReceiveGoodRates() {
+        vehicle.receiveGoodRate();
+        vehicle.receiveGoodRate();
+        assertEquals(vehicle.getRate(), 2);
+    }
+
+    @Test
+    public void itShouldBeAbleToReceiveBadRates() {
+        vehicle.receiveBadRate();
+        assertEquals(vehicle.getRate(), -1);
+    }
 }
