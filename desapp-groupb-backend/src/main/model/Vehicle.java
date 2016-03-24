44af0cd1bc2acb6c594462a742b1a95747c2e0f1
camilagarcia113 <camilagarcia.113@gmail.com>
2016-03-24 19:28:18 -0300

    Added first tests and basic behaviour for Vehicle

--- desapp-groupb-backend/src/main/model/Vehicle.java
+++ desapp-groupb-backend/src/main/model/Vehicle.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Vehicle {
+
+	private int capacity, rate;
+	
+	public Vehicle(int capacity){
+		this.capacity = capacity;
+		rate = 0;
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public void receiveGoodRate() {
+		rate += 1;
+	}
+	
+	public void receiveBadRate() {
+		rate -= 1;
+	}
+	
+	public int getRate() {
+		return rate;
+	}
 }
