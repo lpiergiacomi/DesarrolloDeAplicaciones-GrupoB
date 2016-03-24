b7af61b2df535bffd2d3c1034db69a3f60623bbc
angeles-tellaarena <angeles.tellaarena@gmail.com>
2016-03-24 18:18:14 -0300

    First tests for User

--- desapp-groupb-backend/src/main/model/User.java
+++ desapp-groupb-backend/src/main/model/User.java
@@ -1,9 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
+	private Vehicle vehicle;
+	private List<Route> routes = new ArrayList<Route>();
+	private List<RideRequest> rideRequests = new ArrayList<RideRequest>();
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public List<Route> getRoutes(){
+		return routes;
+	}
+
+	public List<RideRequest> getRideRequests(){
+		return rideRequests;
+	}
+
 	public boolean hasVehicle() {
-		return true;
+		return vehicle != null;
+	}
+
+	public void addRoute(Route route){
+		routes.add(route);
+	}
+
+	public void addRideRequest(RideRequest rideRequest){
+		rideRequests.add(rideRequest);
 	}
 
 }
--- desapp-groupb-backend/src/test/model/UserTest.java
+++ desapp-groupb-backend/src/test/model/UserTest.java
@@ -1,13 +1,44 @@
 package model;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
 
+	public User user;
+	public Vehicle vehicle;
+
+	@Before
+	public void setUp(){
+		user = new User();
+		vehicle = mock(Vehicle.class);
+	}
+
 	@Test
-	public void itShouldCheckIfAUserHasAVehicle() {
-		User user = new User();
+	public void itShouldAssertTheUserHasAVehicle() {
+		user.setVehicle(vehicle);
 		assertTrue(user.hasVehicle());
+	}
+
+	@Test
+	public void itShouldDenyTheUserHasAVehicle(){
+		assertFalse(user.hasVehicle());
+	}
+
+	@Test
+	public void itShouldGetTheUsersRoutes(){
+		Route route = mock(Route.class);
+		user.addRoute(route);
+		assertEquals(user.getRoutes().size(), 1);
+	}
+
+	@Test
+	public void itShouldSendARequestForJoiningADriversRide(){
+		RideRequest rideRequest = mock(RideRequest.class);
+		user.addRideRequest(rideRequest);
+		assertEquals(user.getRideRequests().size(), 1);
 	}
 }
