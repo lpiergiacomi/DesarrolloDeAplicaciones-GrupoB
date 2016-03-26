0d5673f076d5e9b7f94f5d2e91a1248b199ec9fd
javierperini <javierperini90@gmail.com>
2016-03-26 18:13:27 -0300

    prueba, el build deberia romper porque el test falla

--- desapp-groupb-backend/src/test/model/RideTest.java
+++ desapp-groupb-backend/src/test/model/RideTest.java
@@ -14,7 +14,7 @@ public class RideTest {
         Route route = mock(Route.class);
         DateTime date = new DateTime();
         Ride ride = new Ride(drive, route, date);
-        assertEquals(ride.getDrive(), drive);
+        assertEquals(ride.getDrive(), "");
         assertEquals(ride.getRoute(), route);
         assertEquals(ride.getDate(), date);
     }
