5198603bf52df1cd7b6a8d5a2d4c3ad1ae08509f
angeles-tellaarena <angeles.tellaarena@gmail.com>
2016-03-29 22:01:37 -0300

    First tests for RideDate

---
+++ desapp-groupb-backend/src/test/model/DayOfWeekRideDateTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DayOfWeekRideDateTest {
+
+    DayOfWeekRideDate rideDate;
+    DateTime aTuesday = new DateTime(2016, 3, 29, 12, 0);
+
+    @Test
+    public void itShouldAssertAWeekDayIsARideDay(){
+        int tuesday = DateTimeConstants.TUESDAY;
+        rideDate = new DayOfWeekRideDate(tuesday);
+
+        assertTrue(rideDate.isRideDay(aTuesday));
+    }
+
+    @Test
+    public void itShouldDenyAWeekDayIsARideDay(){
+        int wednesday = DateTimeConstants.WEDNESDAY;
+        rideDate = new DayOfWeekRideDate(wednesday);
+
+        assertFalse(rideDate.isRideDay(aTuesday));
+    }
+}
