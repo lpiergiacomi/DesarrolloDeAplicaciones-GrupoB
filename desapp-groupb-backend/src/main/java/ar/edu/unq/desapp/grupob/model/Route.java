924a74d545863132a66bd3f84a7c6e6c8dbbfeda
camilagarcia113 <camilagarcia.113@gmail.com>
2016-04-09 00:47:47 -0300

    Added route basic behaviour

--- desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/model/Route.java
+++ desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/model/Route.java
@@ -1,4 +1,20 @@
 package ar.edu.unq.desapp.grupob.model;
 
 public class Route {
+
+    private double latitude;
+    private double longitude;
+
+    public Route(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
