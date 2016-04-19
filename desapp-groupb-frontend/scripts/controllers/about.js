adbdf2af6807c556c1e4a25bdf414633492d4540
angeles-tellaarena <angeles.tellaarena@gmail.com>
2016-04-19 19:01:58 -0300

    First commit for frontend project

---
+++ desapp-groupb-frontend/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the holisApp
+ */
+angular.module('holisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ desapp-groupb-frontend/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the holisApp
+ */
+angular.module('holisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
