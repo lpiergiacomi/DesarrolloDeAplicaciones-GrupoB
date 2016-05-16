27f8fd76fcca9330c59800b5d0ab29271ddd2eeb
camilagarcia113 <camilagarcia.113@gmail.com>
2016-05-15 22:36:59 -0300

    AspectJ for logging spring services added

--- desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
+++ desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
@@ -3,17 +3,20 @@ package ar.edu.unq.desapp.grupob.services;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.joda.time.DateTime;
 
 @Aspect
 public class LoggingAspect {
 
-    @After("execution(* ar.edu.unq.desapp.grupob.services.GenericService.*(..)")
+    @After("execution(* ar.edu.unq.desapp.grupob.services.GenericService.*(..))")
     public void logBefore(JoinPoint joinPoint) {
 
-        System.out.println("==========================================================");
-        System.out.println("==========================================================");
-        System.out.println("=====================HOLISSSSSSSSSSSSSSS  ================");
-        System.out.println("==========================================================");
-        System.out.println("==========================================================");
+        System.out.println("===================================================================");
+        System.out.println("===================================================================");
+        System.out.println("At: " + new DateTime());
+        System.out.println("In the class " + joinPoint.getSignature().getDeclaringType());
+        System.out.println("The method " + joinPoint.getSignature().getName() + " was called");
+        System.out.println("===================================================================");
+        System.out.println("===================================================================");
     }
 }
