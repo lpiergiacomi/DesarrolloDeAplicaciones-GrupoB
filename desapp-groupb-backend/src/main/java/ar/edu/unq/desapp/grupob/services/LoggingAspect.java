10e0993f94b1b4032faffe4e18ab8b1f13f58e58
camilagarcia113 <camilagarcia.113@gmail.com>
2016-05-16 19:52:30 -0300

    Changed some aspect logging calls

--- desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
+++ desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
@@ -10,13 +10,10 @@ public class LoggingAspect {
 
     @After("execution(* ar.edu.unq.desapp.grupob.services.GenericService.*(..))")
     public void logBefore(JoinPoint joinPoint) {
-
-        System.out.println("===================================================================");
-        System.out.println("===================================================================");
+        System.out.println("***********");
         System.out.println("At: " + new DateTime());
-        System.out.println("In the class " + joinPoint.getSignature().getDeclaringType());
+        System.out.println("In the class " + joinPoint.getTarget().toString());
         System.out.println("The method " + joinPoint.getSignature().getName() + " was called");
-        System.out.println("===================================================================");
-        System.out.println("===================================================================");
+        System.out.println("***********");
     }
 }
