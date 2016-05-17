c705a2a779c56c6f10c34bcd704f57704e660764
camilagarcia113 <camilagarcia.113@gmail.com>
2016-05-17 20:20:29 -0300

    Logger finished

--- desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
+++ desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/LoggingAspect.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desapp.grupob.services;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,12 +9,22 @@ import org.joda.time.DateTime;
 @Aspect
 public class LoggingAspect {
 
+    public static Logger logger = Logger.getLogger(LoggingAspect.class);
+
     @After("execution(* ar.edu.unq.desapp.grupob.services.GenericService.*(..))")
     public void logBefore(JoinPoint joinPoint) {
-        System.out.println("***********");
-        System.out.println("At: " + new DateTime());
-        System.out.println("In the class " + joinPoint.getTarget().toString());
-        System.out.println("The method " + joinPoint.getSignature().getName() + " was called");
-        System.out.println("***********");
+        logger.info("AT: " + new DateTime());
+        logger.info("CLASS: " + joinPoint.getTarget().toString() +
+                    " | METHOD: " + joinPoint.getSignature().getName() +
+                    " | ARGUMENTS: " + printArguments(joinPoint.getArgs()));
+    }
+
+    private static String printArguments(Object[] anArgumentsArray) {
+        Object[] arguments = anArgumentsArray;
+        String output = "";
+        for (Object argument: arguments) {
+            output += argument;
+        }
+        return output;
     }
 }
