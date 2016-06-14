352c44bd2c44416fe1169e049de680afaad5106c
camilagarcia113 <camilagarcia.113@gmail.com>
2016-06-14 19:31:08 -0300

    Adding missing RideService

---
+++ desapp-groupb-backend/src/main/java/ar/edu/unq/desapp/grupob/services/RideService.java
@@ -0,0 +1,10 @@
+package ar.edu.unq.desapp.grupob.services;
+
+import javax.ws.rs.Path;
+
+import ar.edu.unq.desapp.grupob.model.Ride;
+
+@Path("/rides")
+public class RideService extends GenericService<Ride> {
+
+}
