081095d19cb6974a658a03b1238a6cc42cc1345c
camilagarcia113 <camilagarcia.113@gmail.com>
2016-06-20 16:50:14 -0300

    Adding google maps + routes service + routes persistence

--- desapp-groupb-frontend/scripts/controllers/MapController.js
+++ desapp-groupb-frontend/scripts/controllers/MapController.js
@@ -1,125 +1,136 @@
-// angular.module("subiQueTeLlevoApp", [])
-// .controller("MapController", function ($scope, $http, $rootScope, $window) {
+angular.module("subiQueTeLlevoApp")
+.controller("MapController", function ($scope, $http, $rootScope) {
 
-    function initMap() {
+    $scope.routeUrl = $rootScope.baseUrl +"/routes/"
+    
+    $scope.fetchDrawnRoute = function() {
+        $scope.originLatitude = localStorage.getItem("originLatitude");
+        $scope.originLongitude = localStorage.getItem("originLongitude");
+        $scope.destinationLatitude = localStorage.getItem("destinationLatitude");
+        $scope.destinationLongitude = localStorage.getItem("destinationLongitude");
+    };
 
-        var originPlaceId = null;
-        var destinationPlaceId = null;
-        var travelMode = google.maps.TravelMode.DRIVING;
-        var buenosAires = {
-            lat : -34.6261939,
-            lng : -58.410998
-        };
-        var map = new google.maps.Map(document.getElementById("map"), {
-            mapTypeControl : false,
-            center : buenosAires,
-            zoom : 13
-        });
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        directionsDisplay.setMap(map);
+    $scope.fetchDrawnRoute();
 
-        var origin_input = document.getElementById("origin-input");
-        var destination_input = document.getElementById("destination-input");
+    $scope.saveRoute = function() {
+        $http.post($scope.routeUrl, 
+            {
+                "begin": {"latitude": $scope.originLatitude, "longitude": $scope.originLongitude },
+                "end": {"latitude": $scope.destinationLatitude, "longitude": $scope.destinationLongitude }
+            }).success(function(data) {
+                console.log("Route saved successfully");
+            });
+    };
 
-        var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
-        origin_autocomplete.bindTo("bounds", map);
-        var destination_autocomplete = new google.maps.places.Autocomplete(
-                destination_input);
-        destination_autocomplete.bindTo("bounds", map);
+    // $scope.searchRoute = function() {
 
-        function expandViewportToFitPlace(map, place) {
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(20);
-            }
+    // };
+});
+
+function initMap() {
+
+    var originPlaceId = null;
+    var destinationPlaceId = null;
+    var travelMode = google.maps.TravelMode.DRIVING;
+    var buenosAires = {
+        lat : -34.6261939,
+        lng : -58.410998
+    };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        mapTypeControl : false,
+        center : buenosAires,
+        zoom : 13
+    });
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+
+    var geocoder = new google.maps.Geocoder;
+
+    directionsDisplay.setMap(map);
+
+    var origin_input = document.getElementById("origin-input");
+    var destination_input = document.getElementById("destination-input");
+
+    var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+    origin_autocomplete.bindTo("bounds", map);
+    var destination_autocomplete = new google.maps.places.Autocomplete(
+            destination_input);
+    destination_autocomplete.bindTo("bounds", map);
+
+    function expandViewportToFitPlace(map, place) {
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(20);
         }
+    }
 
-        origin_autocomplete.addListener("place_changed", function() {
-            var place = origin_autocomplete.getPlace();
-            if (!place.geometry) {
-                window.alert("Autocomplete's returned place contains no geometry");
-                return;
-            }
-            expandViewportToFitPlace(map, place);
+    origin_autocomplete.addListener("place_changed", function() {
+        var place = origin_autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        expandViewportToFitPlace(map, place);
 
-            originPlaceId = place.place_id;
-            route(originPlaceId, destinationPlaceId, travelMode,
-                    directionsService, directionsDisplay);
-        });
+        originPlaceId = place.place_id;
+        route(originPlaceId, destinationPlaceId, travelMode,
+                directionsService, directionsDisplay);
+    });
 
-        destination_autocomplete.addListener("place_changed", function() {
-            var place = destination_autocomplete.getPlace();
-            if (!place.geometry) {
-                window.alert("Autocomplete's returned place contains no geometry");
-                return;
-            }
-            expandViewportToFitPlace(map, place);
+    destination_autocomplete.addListener("place_changed", function() {
+        var place = destination_autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+        expandViewportToFitPlace(map, place);
 
-            destinationPlaceId = place.place_id;
-            route(originPlaceId, destinationPlaceId, travelMode,
-                    directionsService, directionsDisplay);
-        });
+        destinationPlaceId = place.place_id;
+        route(originPlaceId, destinationPlaceId, travelMode,
+                directionsService, directionsDisplay);
+    });
 
-        function route(originPlaceId, destinationPlaceId, travelMode,
-                directionsService, directionsDisplay) {
-            if (!originPlaceId || !destinationPlaceId) {
-                return;
-            }
-            directionsService.route({
-                origin : {
-                    "placeId" : originPlaceId
-                },
-                destination : {
-                    "placeId" : destinationPlaceId
-                },
-                travelMode : travelMode
-            }, function(response, status) {
+    function route(originPlaceId, destinationPlaceId, travelMode,
+            directionsService, directionsDisplay) {
+        if (!originPlaceId || !destinationPlaceId) {
+            return;
+        }
+        directionsService.route({
+            origin : {
+                "placeId" : originPlaceId
+            },
+            destination : {
+                "placeId" : destinationPlaceId
+            },
+            travelMode : travelMode
+        }, function(response, status) {
                 if (status === google.maps.DirectionsStatus.OK) {
                     directionsDisplay.setDirections(response);
+                    geocodeAddressOrigin();
+                    geocodeAddressDestination();
                 } else {
                     window.alert("Directions request failed due to " + status);
                 }
-            });
-        }
+        });
     }
-// });
-/*
 
-
-                    var geocoder = new google.maps.Geocoder;
-                    geocoder.geocode({'placeId': directionsDisplay.getDirections().geocoded_waypoints[0].place_id}, function(results, status) {
-                        if (status === google.maps.GeocoderStatus.OK) {
-                          if (results[0]) {
-                            console.log(results[0].geometry.location.lat());
-                            console.log(results[0].geometry.location.lng());
-                            console.log(results[0].formatted_address);
-                            "http://localhost:8080/sqtl/routes"
-                          } else {
-                            window.alert('No results found');
-                          }
-                        } else {
-                          window.alert('Geocoder failed due to: ' + status);
-                        }
-                    });
-
-                    
-    $scope.saveProductForm= function(product){
-        $http.post($scope.productUrl,
-                {"name": product.name, "stock": product.stock, "cost": product.cost})
-        .success(function(data){
-            $scope.productNew = {"name": "", "stock":"","cost":""};
-            $scope.hideProductForm = true;
-            $scope.getAllProducts();
+    function geocodeAddressOrigin() {
+        geocoder.geocode({'placeId': directionsDisplay.getDirections().geocoded_waypoints[0].place_id}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                localStorage.setItem("originLatitude", results[0].geometry.location.lat());
+                localStorage.setItem("originLongitude", results[0].geometry.location.lng());
+            }
         });
     };
 
-    $scope.getAllProducts= function(){
-        $http.get($scope.productUrl + "all")
-            .success(function(data){
-                $scope.allProducts = data;
-            });
+    function geocodeAddressDestination() {
+        geocoder.geocode({'placeId': directionsDisplay.getDirections().geocoded_waypoints[1].place_id}, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                localStorage.setItem("destinationLatitude", results[0].geometry.location.lat());
+                localStorage.setItem("destinationLongitude", results[0].geometry.location.lng());
+            }
+        });
     };
-*/
+}
